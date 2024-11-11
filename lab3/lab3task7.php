77401143c2d34c6b23101736d6058413b804a242
Rs Rafsan <114871990+RsRafsan@users.noreply.github.com>
2024-11-11 13:33:02 +0600

    Update lab3task7.php

--- lab3/lab3task7.php
+++ lab3/lab3task7.php
@@ -1,11 +1,16 @@
 <?php 
+print("<html><table border='1' width='250' cellspacing ='0'><tr>");
+print("<td>");
 for ($i = 1; $i <= 3; $i++){
     for ($j = 1; $j <= $i; $j++){
         echo "* ";
     }
     echo "\n<br>";
 }
+print("</td>");
+echo "\n<br>";
 
+print("<td>");
 for ($i = 3; $i >= 1; $i--){
     for ($j = 1; $j <=$i; $j++){
         echo "$j ";
@@ -13,7 +18,11 @@ for ($i = 3; $i >= 1; $i--){
     echo "\n<br>";
 
 }
+print("</td>");
+echo "\n<br>";
 
+
+print("<td>");
 $char='A';
 for ($i = 1; $i <= 3; $i++){
     for ($j = 1; $j <= $i; $j++){
@@ -23,5 +32,8 @@ for ($i = 1; $i <= 3; $i++){
         
         echo "\n<br>";
     }
+    print("</td>");
+    echo "\n<br>";
 
+    print("</tr></table></html>")
 ?>
